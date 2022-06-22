87fb5b58e463d8b5f106a92451215b84ba9d5489
Dashiell Stander <dstander@protonmail.com>
2022-06-21 21:03:08 -0700

    the model jits and runs

--- src/ssm/model.py
+++ src/ssm/model.py
@@ -22,7 +22,7 @@ class TransformerConfig:
     max_len: int = 512
     dropout_rate: float = 0.1
     attention_dropout_rate: float = 0.1
-    kernel_init: Callable = nn.initializers.xavier_uniform()
+    # kernel_init: Callable = nn.initializers.xavier_uniform()
     fourier_init_std: float = 0.2
 
 
@@ -54,16 +54,18 @@ def apply_rotary_pos_emb(x, sincos):
 
 
 
+"""
 class RMSNorm(nn.Module):
 
     @nn.compact
-    def forward(self, x):
+    def __call__(self, x):
         eps = 1e-8
         dim = x.shape[-1]
-        scale = jnp.pow(dim, -0.5)
-        g = self.param('g', nn.initializers.ones(), dim)
+        scale = jnp.power(dim, -0.5)
+        g = self.param('g', nn.initializers.ones(), (dim,))
         norm = jax.nn.normalize(x, axis=-1) * scale
         return x / jax.lax.clamp(eps, norm) * g
+"""
 
 
 
@@ -74,7 +76,7 @@ class FourierFeatures(nn.Module):
     def __call__(self, x):
         w = self.param(
             'w',
-            nn.initializers.normal(stddev=self.config.fourier_init_std)
+            nn.initializers.normal(stddev=self.config.fourier_init_std),
             (self.config.time_dim // 2, x.shape[1]),
         )
         f = 2 * jnp.pi * x @ w.T
@@ -85,25 +87,22 @@ class SelfAttention(nn.Module):
     config: TransformerConfig
 
     @nn.compact
-    def __call__(self, x, padding_mask=None):
-        x = RMSNorm()(x)
+    def __call__(self, x, padding_mask=None, deterministic=False):
+        x = nn.LayerNorm(use_bias=False, use_scale=False)(x)
         padding_mask = None if padding_mask is None else padding_mask[:, None, None, :]
         x = nn.MultiHeadDotProductAttention(
             self.config.num_heads,
             dropout_rate=self.config.attention_dropout_rate
-        )(x, x, padding_mask)
+        )(x, x, padding_mask, deterministic=deterministic)
         return x
 
 
 class FeedForward(nn.Module):
     config: TransformerConfig
 
-    def __init__(self, d_ff, name=None):
-        super().__init__(name=name)
-        self.d_ff = d_ff
-
+    @nn.compact
     def __call__(self, x):
-        x = RMSNorm()(x)
+        x = nn.LayerNorm(use_bias=False, use_scale=False)(x)
         x = nn.Dense(self.config.mlp_dim, use_bias=False)(x)
         x = nn.gelu(x)
         x = nn.Dense(self.config.model_dim, use_bias=False)(x)
@@ -114,13 +113,13 @@ class TransformerLayer(nn.Module):
     config: TransformerConfig
 
     @nn.compact    
-    def __call__(self, x, padding_mask=None):
+    def __call__(self, x, padding_mask=None, deterministic=False):
         #x_rot = x[:, :, :self.d_rotary]
         #x_pass = x[ :, :, self.d_rotary:]
         #sincos = fixed_pos_embedding(x_rot, seq_dim=1)
         #x_rot = apply_rotary_pos_emb(x_rot, sincos)
         #x = jnp.concatenate([x_rot, x_pass], axis=-1)
-        x = x + SelfAttention(self.config)(x, padding_mask)
+        x = x + SelfAttention(self.config)(x, padding_mask, deterministic)
         x = x + FeedForward(self.config)(x)
         return x
 
@@ -140,19 +139,23 @@ class SkipBlock(nn.Module):
 class TransformerDiffusion(nn.Module):
     config: TransformerConfig
     
-    def __call__(self, x, t):
+    @nn.compact
+    def __call__(self, x, t, training=False):
         """
         x.shape = 
         """
+        deterministic = not training
         x_init = nn.Dense(self.config.embed_dim)(x)
-        log_snr = alpha_sigma_to_log_snr(t_to_alpha_sigma(t))
+        log_snr = alpha_sigma_to_log_snr(*t_to_alpha_sigma(t))
         timestep_embed = FourierFeatures(self.config)(log_snr[:, None])
-        te_planes = jnp.tile(timestep_embed[..., None], [x.shape[0], 1])
-        x = jnp.concatenate([x_init, te_planes], axis=1)
-        layers = nn.Sequential( * [
-            TransformerLayer(self.config) for _ in range(self.n_layers)
-        ])
-        x = x + jnp.sqrt(2) * layers(x)
-        x_final = nn.Linear(self.vocab_size)(x)
+        te_planes = jnp.tile(timestep_embed[None], (1, self.config.max_length, 1))
+        x = jnp.concatenate([x_init, te_planes], axis=-1)
+        x = FeedForward(self.config)(x)
+        trans_x = nn.Sequential([
+            TransformerLayer(self.config) for _ in range(self.config.num_layers)
+        ])(x, None, deterministic=deterministic)        
+        x = x + jnp.sqrt(2) * trans_x
+        x_final = nn.Dense(self.config.vocab_size)(x)
         x_var = jnp.var(x_final, axis=-1)
-        return x_final / x_var
+        return x_final / x_var[..., None]
+
