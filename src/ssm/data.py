637dc513c879009ec7ec34f52b8a593dd84670d2
Dashiell Stander <dstander@protonmail.com>
2022-08-05 00:42:50 -0400

    cosine schedule, fix kl div

--- src/ssm/data.py
+++ src/ssm/data.py
@@ -105,18 +105,3 @@ def get_datasets(config, seed):
         .prefetch(2)
     )
     return train_data, val_data
-
-
-def dataloader(path, batch_size, random_seed):
-    files = list(path.glob('*.npz'))
-    npgen = np.random.default_rng(random_seed)
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        data_chunks = executor.map(load_tokens, files)
-        for future in as_completed(data_chunks):
-            data = future.result()
-            data = npgen.shuffle(data['tokens'])
-            splits = list(range(batch_size, data.shape[0], batch_size))
-            for batch in np.split(data, splits):
-                if batch.shape[0] == batch_size:
-                    yield jnp.asarray(batch)
-
