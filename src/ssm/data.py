cosine schedule, fix kl div

src/ssm/data.py
@@ -57,19 +57,25 @@ def tokenize_and_split(tokenizer, seq_len, batch):
 
 
 def load_tokens(fp):
-    with gfile.GFile(fp, mode='rb') as fp:
-        data = np.load(fp)
-    return tf.data.Dataset.from_tensor_slices(data)
+    with gfile.GFile(fp.decode('utf-8'), mode='rb') as file:
+        data = np.load(file)
+    return data
 
 
 def get_datasets(config, seed):
     random.seed(seed)
-    train_data = tf.data.Dataset.list_files(f'{config.data.dataset_path}/train/*.npy', shuffle=True)
-    val_data= tf.data.Dataset.list_files(f'{config.data.dataset_path}/val/*.npy')
+    train_files = gfile.glob(f'{config.data.dataset_path}/train/*.npy')
+    val_files = gfile.glob(f'{config.data.dataset_path}/val/*.npy')
+    random.shuffle(train_files)
+    train_data = tf.data.Dataset.from_tensor_slices(train_files)
+    val_data= tf.data.Dataset.from_tensor_slices(val_files)
     prob_transformer = TokenToProbTransformer(seed, config)
     train_data = (
-        train_data
-        .interleave(load_tokens, cycle_length=10, num_parallel_calls=5)
+        train_data.map(
+            lambda fp: tf.numpy_function(func=load_tokens, inp=[fp], Tout=np.uint16),
+            num_parallel_calls=10
+        )
+        .unbatch()
         .shuffle(10_000)
         .batch(config.data.batch_size, drop_remainder=True)
         .map(
@@ -82,7 +88,11 @@ def get_datasets(config, seed):
         .prefetch(4)
     )
     val_data = (
-        val_data
+        val_data.map(
+            lambda fp: tf.numpy_function(func=load_tokens, inp=[fp], Tout=np.uint16),
+            num_parallel_calls=5
+        )
+        .unbatch()
         .batch(config.data.batch_size, drop_remainder=True)
         .map(
             lambda x: tf.numpy_function(

src/ssm/loss.py
@@ -5,14 +5,19 @@ import ssm.aitchison as aitch
 from ssm.utils import t_to_alpha_sigma
 
 
-def kl(p, q, eps: float = 2 ** -16):
-    return p.dot(jnp.log(p + eps) - jnp.log(q + eps))
+@jax.vmap
+def kl(p, q):
+    eps: float = 2 ** -16
+    return jnp.tensordot(
+        p,
+        jnp.log(p + eps) - jnp.log(q + eps)
+    )
 
 
-def jsd(p, q, eps: float = 2. ** -16):
-    m = (p + q) / 2
-    pm = kl(p, m, eps)
-    qm = kl(q, m, eps)
+def jsd(p, q):
+    m = jnp.add(p, q) / 2
+    pm = kl(p, m)
+    qm = kl(q, m)
     return (pm + qm) / 2
 
 
@@ -71,11 +76,10 @@ def kl_denoising(params, state, x0, xt, key):
     return kl_div + score_norm
 
 
-def jsd_denoising(params, state, x0, xt, key):
-    keys = jax.random.split(key, 3)
+def jsd_denoising(params, state, x0, xt, t, keys):
     batch_dim, seq_len, simplex_dim = x0.shape
     t = jax.random.uniform(keys[0], (batch_dim,))
-    score = state.apply({'params': params},  xt, t, rngs={'dropout': keys[0]})
+    score = state.apply_fn({'params': params},  xt, t, rngs={'dropout': keys[1]})
     #score_norm = jnp.power(score, 2).sum()
-    js_div = jsd(softmax(x0), softmax(score))
-    return js_div
+    js_div = jsd(softmax(x0, axis=-1), softmax(score, axis=-1))
+    return (t * js_div).sum()

train.py
@@ -11,7 +11,6 @@ from omegaconf import OmegaConf
 import optax
 import os
 from pathlib import Path
-import tensorflow as tf
 import time
 from tqdm import tqdm, trange
 import wandb
@@ -69,6 +68,10 @@ def eval_model(state, texts, noised_texts, t, key):
 @partial(jax.pmap, axis_name='batch')
 def update_model(state, grads):
     return state.apply_gradients(grads=grads)
+
+
+def get_times(batch_size, key):
+    return jnp.cos(jax.random.uniform(key, (batch_size,)) * (jnp.pi / 2))
 
 
 def demo():
@@ -160,15 +163,8 @@ def main(args):
             key, time_key, sde_key, *local_keys = rng_split(key, 3 + (2 * num_local_devices))
             sde_keys = psplit(jnp.stack(rng_split(sde_key, batch_size)), num_local_devices)
             texts = psplit(batch, num_local_devices)
-            times = psplit(
-                jax.random.uniform(
-                    time_key,
-                    (batch_size,),
-                    minval=config.sde.start_time,
-                    maxval=config.sde.end_time),
-                num_local_devices
-            )
-            noised_texts = forward_noising(texts, times, sde_keys)
+            times = psplit(get_times(batch_size, time_key), num_local_devices)
+            noised_texts = forward_noising(texts, times * config.sde.end_time, sde_keys)
             forward_sde_time = time.time() - batch_start
             times = times / config.sde.end_time
             loss, grads = apply_model(state, texts, noised_texts, times, psplit(jnp.stack(local_keys), num_local_devices))