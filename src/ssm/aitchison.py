cosine schedule, fix kl div

src/ssm/aitchison.py
@@ -44,7 +44,7 @@ def clr_inv(x: chex.Array, axis=-1, initial=None) -> chex.Array:
     return jax.nn.softmax(x, axis=axis, initial=initial)
 
 
-def clr(x: chex.Array, axis: int = -1, keepdims: bool = False) -> chex.Array:
+def clr(x, axis: int = -1, keepdims: bool = False):
     """ Centered log ration (clr) transform of a point on the simplex. Takes a point in the canonical basis to 
     """
     log_x = jnp.log(x)

src/ssm/data.py
@@ -1,4 +1,3 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
 import numpy as np
 import jax
 import jax.numpy as jnp
@@ -85,7 +84,7 @@ def get_datasets(config, seed):
             ),
             num_parallel_calls=8
         )
-        .prefetch(4)
+        .prefetch(2)
     )
     val_data = (
         val_data.map(
@@ -93,6 +92,7 @@ def get_datasets(config, seed):
             num_parallel_calls=5
         )
         .unbatch()
+        .shuflle(10_000)
         .batch(config.data.batch_size, drop_remainder=True)
         .map(
             lambda x: tf.numpy_function(
@@ -100,7 +100,7 @@ def get_datasets(config, seed):
                 inp=[x],
                 Tout=np.float32
             ), 
-            num_parallel_calls=8
+            num_parallel_calls=4
         )
         .prefetch(2)
     )

src/ssm/loss.py
@@ -78,8 +78,9 @@ def kl_denoising(params, state, x0, xt, key):
 
 def jsd_denoising(params, state, x0, xt, t, keys):
     batch_dim, seq_len, simplex_dim = x0.shape
+    xt = jax.nn.normalize(xt, axis=-1)
     t = jax.random.uniform(keys[0], (batch_dim,))
     score = state.apply_fn({'params': params},  xt, t, rngs={'dropout': keys[1]})
-    #score_norm = jnp.power(score, 2).sum()
+    score_norm = jnp.power(score, 2).sum()
     js_div = jsd(softmax(x0, axis=-1), softmax(score, axis=-1))
-    return (t * js_div).sum()
+    return (t * js_div).sum() + score_norm

src/ssm/model.py
@@ -7,8 +7,6 @@ import jax.numpy as jnp
 import numpy as np
 from typing import Sequence
 
-from ssm.utils import alpha_sigma_to_log_snr, t_to_alpha_sigma
-
 
 @struct.dataclass
 class TransformerConfig:
@@ -152,10 +150,8 @@ class TransformerDiffusion(nn.Module):
         x.shape = 
         """
         deterministic = not training
-        #x = normalize_probabilities(x)
         x_init = nn.Dense(self.config.embed_dim)(x)
-        log_snr = alpha_sigma_to_log_snr(*t_to_alpha_sigma(t))
-        timestep_embed = FourierFeatures(self.config)(log_snr[:, None])
+        timestep_embed = FourierFeatures(self.config)(t[:, None])
         te_planes = jnp.tile(timestep_embed[:, None], (1, self.config.max_length, 1))
         x = jnp.concatenate([x_init, te_planes], axis=-1)
         x = FeedForward(self.config)(x)
@@ -164,9 +160,7 @@ class TransformerDiffusion(nn.Module):
         ])(x, None, deterministic=deterministic)        
         x = x + jnp.sqrt(2) * trans_x
         x = nn.Dense(self.config.vocab_size)(x)
-        x_final = x - jnp.mean(x, axis=-1, keepdims=True)
-        x_final = x_final / jnp.var(x_final, axis=-1, keepdims=True)
-        return x_final
+        return jax.nn.normalize(x, axis=-1)
 
 
 def create_train_state(rng, config, optimizer):
@@ -182,7 +176,6 @@ def create_train_state(rng, config, optimizer):
         config.data.seq_len,
         config.model.dropout,
         config.model.attention_dropout
-
     )
 
     model = TransformerDiffusion(transformer_config)

src/ssm/sde.py
@@ -27,7 +27,7 @@ def dirichlet_potential(p, alphas):
 
 
 def drift_potential(t, x, args):
-    x = x / jnp.var(x, axis=-1, keepdims=True)
+    x = jax.nn.normalize(x, axis=-1)
     p = jax.nn.softmax(x)
     alphas = jnp.ones((p.shape[-1],))
     return -1 * aitch.simplex_metric_tensor_inv(
@@ -49,11 +49,10 @@ def softmax_jac_squared(x):
 
 
 def reverse_drift(model_fn, t, x, args):
+    x = jax.nn.normalize(x, axis=-1)
     drift = drift_potential(t, x, {})
     score = jnp.squeeze(model_fn(x, t))
     j_sfm = softmax_jac_squared(x)
-    #print(j_sfm.shape)
-    #print(score.shape)
     return drift - (0.5 * jnp.einsum('sij,si->sj', j_sfm, score))
 
 
@@ -62,23 +61,22 @@ def dirichlet_forward_sde(x0, t1, key):
     """ The forward noising SDE that converges to Dir([1, 1, ...., 1]) (i.e. uniform on the simplex)
     """
     t0 = 0
-    brownian_motion = VirtualBrownianTree(t0, t1, tol=0.05, shape=x0.shape, key=key)
+    brownian_motion = VirtualBrownianTree(t0, t1, tol=0.009, shape=x0.shape, key=key)
     terms = MultiTerm(ODETerm(drift_potential), SimplexControlTerm(lambda t, y, args: y, brownian_motion))
     solver = Euler() 
-    sol = diffeqsolve(terms, solver, t0, t1, dt0=0.1, y0=x0)
+    sol = diffeqsolve(terms, solver, t0, t1, dt0=0.01, y0=x0)
     return sol.ys[0]
 
 
-@partial(jax.vmap, in_axes=(None, None, 0, 0))
-def dirichlet_reverse_sde(model, size, t1, key):
+def dirichlet_reverse_sde(model, size, t1, keys):
 
     def drift(t, x, args):
         return reverse_drift(model, t, x, args)
 
-    t0 = 1e-8
-    init_key, dw_key = jax.random.split(key)
-    x1 = jax.random.dirichlet(init_key, jnp.ones(size))
-    brownian_motion = VirtualBrownianTree(t1, t0, tol=0.05, shape=x1.shape, key=dw_key)
+    t0 = 1e-16
+    #init_key, dw_key = jax.random.split(key)
+    x1 = jax.random.dirichlet(keys[0], jnp.ones(size))
+    brownian_motion = VirtualBrownianTree(t1, t0, tol=0.05, shape=x1.shape, key=keys[1])
     terms = MultiTerm(ODETerm(drift), SimplexControlTerm(brownian_motion))
     solver = Euler()
     sol = diffeqsolve(terms, solver, t1, t0, dt0=-0.1, y0=aitch.clr(x1, axis=-1, keepdims=True))

train_sliced_sm.py
@@ -17,10 +17,10 @@ from tqdm import tqdm, trange
 import wandb
 
 import ssm.aitchison as aitch
-from ssm.data import TokenToProbTransformer
+from ssm.data import get_datasets, sample
 from ssm.loss import sliced_score_matching
 from ssm.model import create_train_state
-from ssm.sde import dirichlet_forward_sde
+from ssm.sde import dirichlet_forward_sde, dirichlet_reverse_sde
 from ssm.utils import ema_update, psplit, tree_bytes, tree_size
 
 
@@ -38,46 +38,14 @@ p.add_argument('--run-name', type=str, default='SSM Diffusion')
 p.add_argument('--checkpoint-dir', type=str, default='checkpoints')
 
 
-
-def get_datasets(config, seed):
-    prob_transformer = TokenToProbTransformer(seed, config)
-    data = np.lib.format.open_memmap(config.data.dataset_path, mode='r')
-    train_data = tf.data.Dataset.from_tensor_slices(data[:-config.data.val_size, :])
-    val_data = tf.data.Dataset.from_tensor_slices(data[-config.data.val_size:, :])
-    train_data = (
-        train_data
-        .shuffle(10_000)
-        .batch(config.data.batch_size, drop_remainder=True)
-        .map(
-            lambda x: tf.numpy_function(
-                func=prob_transformer, inp=[x],
-                Tout=np.float32
-            ),
-            num_parallel_calls=8
-        )
-        .prefetch(4)
-    )
-    val_data = (
-        val_data
-        .batch(config.data.batch_size, drop_remainder=True)
-        .map(
-            lambda x: tf.numpy_function(
-                func=prob_transformer,
-                inp=[x],
-                Tout=np.float32
-            ), 
-            num_parallel_calls=8
-        )
-        .prefetch(2)
-    )
-    return train_data, val_data, data[:-config.data.val_size, :].shape[0]
-
-
 def save(state, ema_params, dir, index):
     if jax.process_index() == 0:
         checkpoints.save_checkpoint(dir / 'train', jax_utils.unreplicate(state), index)
         checkpoints.save_checkpoint(dir / 'ema', jax_utils.unreplicate(ema_params), index)
 
+
+def get_times(batch_size, key):
+    return jnp.cos(jax.random.uniform(key, (batch_size,)) * (jnp.pi / 2))
 
 
 @partial(jax.pmap, axis_name='batch')
@@ -104,6 +72,19 @@ def eval_model(state, noised_texts, t, key):
 @partial(jax.pmap, axis_name='batch')
 def update_model(state, grads):
     return state.apply_gradients(grads=grads)
+
+
+@partial(jax.pmap, axis_name='batch')
+def demo(state, keys):
+    def score_fn(xt, t):
+        return state.apply_fn(
+            {'params': state.params},
+            xt[None],
+            t[None],
+            False,
+            rngs={'dropout': jax.random.PRNGKey(0)}
+        )
+    sol = dirichlet_reverse_sde(score_fn, (512, 8192), 10., keys)
 
 
 def make_optimizer(config):
@@ -170,7 +151,7 @@ def main(args):
 
     def do_eval(state, key):
         eval_loss = []
-        for batch in tqdm(val_data.as_numpy_iterator()):
+        for batch in tqdm(val_data.shuffle(800).take(100).as_numpy_iterator()):
             key, time_key, sde_key, *local_keys = rng_split(key, 3 + (2 * num_local_devices))
             sde_keys = psplit(jnp.stack(rng_split(sde_key, batch_size)), num_local_devices)
             texts = psplit(batch, num_local_devices)
@@ -186,14 +167,11 @@ def main(args):
         epoch_losses = []
         i = num_steps
         for batch in tqdm(train_data.as_numpy_iterator(), total=train_size // batch_size):
-            if batch.shape[0] != batch_size:
-                continue
             batch_start = time.time()
             key, time_key, sde_key, *local_keys = rng_split(key, 3 + (2 * num_local_devices))
             sde_keys = psplit(jnp.stack(rng_split(sde_key, batch_size)), num_local_devices)
             texts = psplit(batch, num_local_devices)
-            times = psplit(
-                jax.random.uniform(time_key, (batch_size,), minval=config.sde.start_time, maxval=config.sde.end_time), num_local_devices)
+            times = psplit(get_times(batch_size, time_key), num_local_devices)
             noised_texts = forward_noising(texts, times, sde_keys)
             forward_sde_time = time.time() - batch_start
             times = times / config.sde.end_time