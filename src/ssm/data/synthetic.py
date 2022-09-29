0b4b36f7889db2366267074e884a1944a45e5743
Dashiell Stander <dstander@protonmail.com>
2022-09-29 15:11:03 -0400

    Synthetic bigram data
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

---
+++ src/ssm/data/synthetic.py
@@ -0,0 +1,41 @@
+
+from random import choice, choices
+import jax
+import jax.numpy as jnp
+
+
+class BiGramSampler:
+    vocab = ('a', 'b', 'c')
+    
+    probs = {
+        'a': (1./3, 1./3, 1.3),
+        'b': (0.1, 0.4, 0.5),
+        'c': (0.6, 0.2, 0.2)
+    }
+
+    vectors = {
+        'a': jnp.array([1., 0., 0.]),
+        'b': jnp.array([0., 1., 0.]),
+        'c': jnp.array([0., 0., 1.])
+    }
+    
+    def __init__(self, seq_len: int):
+        self.seq_len = seq_len
+        
+    def _sample(self):
+        output = [choice(self.vocab)]
+        for _ in range(self.seq_len - 1):
+            output += choices(self.vocab, weights=self.probs[output[-1]])
+        return ''.join(output)
+        
+    def sample(self, n: int):
+        return [self._sample() for _ in range(n)]
+
+    def to_jax(self, s):
+        return [self.vectors[char] for char in s]
+
+    def make_batch(self, n: int):
+        tensors = [
+            self.to_jax(self._sample()).unsqueeze(0) for _ in range(n)
+        ]
+        return jnp.stack(tensors)
